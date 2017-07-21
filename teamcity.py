1136f75f0bcc808f43d2a834eb479f63a197da2e
Mariusz Makowski <mariusz.makowski@comp-ci.pl>
2017-07-21 13:00:17 +0200

    rest viewr for teamcity

---
+++ teamcity.py
@@ -0,0 +1,22 @@
+import requests
+import base64
+import fire
+import json
+
+user = 'makowsm'
+password = ''  # base64 encoded
+host = 'http://chepri.comp-ci.net:8111/'
+
+
+class teamcity(object):
+    def list_projects(self, project_id='MP_APP'):
+        url = host + "app/rest/projects/id:{!s}".format(project_id)
+        r = requests.get(
+            url,
+            auth=(user, base64.b64decode(password).decode('utf-8')),
+            headers={'accept': 'application/json'})
+        the_dict = json.loads(r.text)
+        return the_dict
+
+if __name__ == '__main__':
+    fire.Fire(teamcity)
